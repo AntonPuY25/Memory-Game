bfcab0767c09ee728889ba50a9c1b55910dd1c0d
Anton Kliaschonak <PuY25@bk.ru>
2022-02-17 23:25:58 +0300

    feat: add score table

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const area = document.querySelector('.area');
 const box = document.querySelector('.box');
 const boxes = document.querySelectorAll('.box')
+const table = document.querySelector('table')
 let step = 0;
 
 const win  = [
@@ -22,18 +23,62 @@ area.addEventListener('click',e =>{
     }
 })
 
+const arr = JSON.parse(localStorage.getItem('ticTacToe')) || [];
+
+
 const countResult = ()=>{
 
     for (let i  =0; i< win.length;i++){
         if(boxes[win[i][0]].innerText === 'X' && boxes[win[i][1]].innerText === 'X' && boxes[win[i][2]].innerText === 'X'){
-            finish('Крестики')
+            finish('Крестики','x')
+            return
         } else if (boxes[win[i][0]].innerText === '0' && boxes[win[i][1]].innerText === '0' && boxes[win[i][2]].innerText === '0'){
-            finish('Нолики')
+            finish('Нолики','0')
+            return;
         }
+    }
+    if(step >= 9){
+        alert('Ничья')
+        if(arr.length === 10){
+            arr.shift()
+        }
+        arr.push({'Крестики':0,'Нолики':0,'Ничья':1})
+        localStorage.setItem('ticTacToe',JSON.stringify(arr))
+        location.reload()
     }
 
 }
-const finish = (winner)=>{
-    alert(`Победили ${winner}`)
+
+
+
+const finish = (winner,item)=>{
+    alert(`Победили ${winner} за ${Math.round(step / 2)} ${ Math.round(step / 2) < 4 ? 'хода': 'ходов'}`)
+    if(arr.length === 10){
+        arr.shift()
+    }
+    if(item === 'x'){
+        arr.push({'Крестики':1,'Нолики':0,'Ничья':0})
+        localStorage.setItem('ticTacToe',JSON.stringify(arr))
+
+    }else if(item === '0'){
+        arr.push({'Крестики':0,'Нолики':1,'Ничья':0})
+        localStorage.setItem('ticTacToe',JSON.stringify(arr))
+    }
         location.reload()
 }
+
+
+arr.forEach(item=>{
+    const tr = document.createElement('tr')
+    const thX = document.createElement('th')
+    thX.textContent = item['Крестики']
+    tr.appendChild(thX)
+    const th0 = document.createElement('th')
+    th0.textContent = item['Нолики']
+    tr.appendChild(th0)
+    const thN = document.createElement('th')
+    thN.textContent = item['Ничья']
+    tr.appendChild(thN)
+    table.appendChild(tr)
+})
+
